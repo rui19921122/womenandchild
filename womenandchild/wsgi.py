fc1ccf56716170f2c98a392a01d94443ed72f433
rui19921122 <rui19921122@qq.com>
2016-01-11 21:19:50 +0800

    添加首页图片修改功能

--- womenandchild/wsgi.py
+++ womenandchild/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "womenandchild.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "womenandchild.serversettings")
 
 application = get_wsgi_application()
